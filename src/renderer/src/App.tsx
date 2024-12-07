ad1d45ee56a92967f0954a0255a6e0e28f504dbb
JohnnnyTang <1275441282@qq.com>
2024-12-07 13:03:31 +0800

    map view mode change

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,17 +1,25 @@
-import Versions from './components/Versions'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import MapComponent from './components/Map'
+// import Versions from './components/Versions'
+import { HashRouter, Routes, Route } from 'react-router'
+import MapComponent from './components/MapEditor'
+import { NavMenu } from './components/layout/NavMenu'
+import { useState } from 'react'
 
 function App(): JSX.Element {
   // const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
+  const RADIO_ITEMS = ['Dark', 'Light']
+  const [radioSelection, setRadioSelection] = useState(RADIO_ITEMS[0])
 
   return (
-    <BrowserRouter>
+    <HashRouter>
+      <NavMenu
+        radioItems={RADIO_ITEMS}
+        selectedMode={radioSelection}
+        onModeChange={setRadioSelection}
+      />
       <Routes>
-        <Route path="/a" element={<Versions />}></Route>
-        <Route path="/" element={<MapComponent />}></Route>
+        <Route path="/" element={<MapComponent viewMode={radioSelection} />}></Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
